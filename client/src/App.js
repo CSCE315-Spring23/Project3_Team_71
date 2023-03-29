bcd3e55f6e6cd97ff7f2e130763d9b926fe4edd7
AnDuongAdonk <anqduong@tamu.edu>
2023-03-29 11:10:34 -0500

    made two routes in server and displayed in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
 import "./css/App.css";
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+
+    const [weather, setWeather] = useState('');
+    const [menu, setMenu] = useState('');
+
+    useEffect(() => {
+        const getWeather = async () => {
+            const res = await fetch('http://localhost:3001/weather/30/30');
+            const data = await res.json();
+
+            setWeather(data);
+        };
+
+        const getMenu = async () => {
+            const res = await fetch('http://localhost:3001/menu');
+            const data = await res.json();
+
+            setMenu(data);
+        }
+        
+        getMenu();
+        getWeather();
+    }, []);
+
+
+
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <header className="App-header">
+                <h1>{JSON.stringify(menu, null, 2)}</h1>
+                <p>
+                    {JSON.stringify(weather, null, 2)}
+                </p>
+            </header>
         </div>
     );
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,44 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'csce315331_team_71_master',
+    host: 'csce-315-db.engr.tamu.edu',
+    database: 'csce315331_team_71',
+    password: '71_TeaM',
+    port: 5432, 
+  });
+
+app.use(cors());
+
+app.get("/weather/:lat/:lon", async (req, res) => {
+    const result = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${req.params.lat}&lon=${req.params.lon}&appid=8055724633e109d30c148d36ea2352b0
+        `
+    );
+
+    const data = await result.json();
+    
+    return res.json(data);
+});
+
+app.get("/menu", async (req, res) => {
+
+    const result = await pool.query('SELECT * FROM menu_items', (err, result) => {
+        if (err) {
+            return res.status(500).send('cant retrieve from db');
+        }
+        else {
+            return res.send(result.rows);
+        }
+    })
+
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port);
+console.log("App is listening on port " + port);
 
-console.log('App is listening on port ' + port);
+
