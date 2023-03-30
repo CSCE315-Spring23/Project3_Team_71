5e34bbc017f1dfed5026223d55483d9bd30cc12e
Alan Nguyen <alansannguyen@client-10-229-227-156.tamulink.tamu.edu>
2023-03-29 19:52:03 -0500

    Added Buttons to Manager

--- client/src/pages/manager/Manager.js
+++ client/src/pages/manager/Manager.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Orders from "./Orders";
 
 const Manager = () => {
 
@@ -16,7 +17,35 @@ const Manager = () => {
         getWeather();
     }, []);
 
-    return <div>{JSON.stringify(weather, null, 2)}</div>;
+    const [showOtherComponent, setShowOtherComponent] = useState(false);
+
+    const handleClick = async () => {
+      setShowOtherComponent(!showOtherComponent);
+    };
+
+    return (
+        <div>
+            <div className="button-row-1">
+                <div>
+                    <button onClick={handleClick}>
+                    {showOtherComponent ? 'Hide Other Component' : 'Show Other Component'}
+                    </button>
+                    {showOtherComponent && <Orders />}
+                </div>
+                <button>Orders</button>
+                <button>Menu Items</button>
+                <button>Ingredient Items</button>
+                <button>Sales Reports</button>
+            </div>
+
+            <div className="button-row-2">
+                <button>Change Menu Items</button>
+                <button>Change Ingredient</button>
+                <button>Add Ingredient</button>
+                <button>Add Menu Item</button>
+            </div>
+        </div>
+    );
 };
 
 export default Manager;
---
+++ client/src/pages/manager/Orders.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+const Orders = () => {
+  return (
+    <div>Orders</div>
+  )
+}
+
+export default Orders
