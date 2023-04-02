dc44480f6e46ad9cf2b8da151812a341efb98c90
Ahnaf-hossain <88912262+Ahnaf-hossain@users.noreply.github.com>
2023-04-02 00:04:50 -0500

    sales with react-table

--- client/src/pages/manager/SalesReports.js
+++ client/src/pages/manager/SalesReports.js
@@ -1,46 +1,71 @@
-import React, { useEffect, useState } from "react";
+//import React, { useEffect, useState, useTable } from "react";
+//import {useTable} from "react-table";
 import "./../../css/sales.css"
+import * as React from "react";
+import { useTable } from "react-table";
+import { useEffect, useState } from "react";
+
+const columns = [
+  {
+    Header: "Sales ID",
+    accessor: "sales_id",
+  },
+  {
+    Header: "Sales Date",
+    accessor: "sales_date",
+  },
+  {
+    Header: "Total Sales",
+    accessor: "total_sales",
+  },
+  {
+    Header: "Total Tax",
+    accessor: "total_tax",
+  },
+];
+
 const SalesReports = () => {
   const [sales, setSales] = useState([]);
+
   useEffect(() => {
     const getSales= async ()=> {
       const res = await fetch ("http://localhost:3001/sales");
-      const getdata= await res.json();
-      setSales(getdata);
-      console.log(getdata);
-      }
-      getSales();
+      const data= await res.json();
+      setSales(data);
+    }
+    getSales();
   }, [])
 
+  const tableInstance = useTable({ columns, data: sales });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
 
   return (
-    <div>
-      <h2>Sales Reports</h2>
     <div className="container">
-    <table className="table table-bordered text-white">
-      <thead>
-        <tr>
-          <th>sales_id</th>
-          <th>sales date</th>
-          <th>total sales</th>
-          <th>total tax</th>
-
-        </tr>
-      </thead>
-      <tbody>
-          {sales.map((sales_list) =>(
-            <tr>
-              <td>{sales_list.sales_id}</td>
-              <td>{sales_list.sales_date}</td>
-              <td>{sales_list.total_sales}</td>
-              <td>{sales_list.total_tax}</td>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
             </tr>
           ))}
-      </tbody>
-    </table>
-    </div>
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
-export default SalesReports
+export default SalesReports;
