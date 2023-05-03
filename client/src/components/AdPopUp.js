349781bb9d40043dc02fe0bd8366cbc9217aeeb2
jbela <73561726+jbela@users.noreply.github.com>
2023-05-02 20:05:14 -0500

    updated adpopup css and descriptions

--- client/src/components/AdPopUp.js
+++ client/src/components/AdPopUp.js
@@ -13,109 +13,108 @@ const AdPopUp = ({ stringID, handleClick, setAdPopUp, showAd }) => {
                 </button>
 
                 <div className="heading">
-                    <h2>Sauces</h2>
+                    <h2>Add Sauce</h2>
                 </div>
 
-                <br/>
 
                 <div className="button-row-cashier">
                     <button
-                        className="btn-cashier"
+                        className="btn-sauce"
                         id="cfaS"
                         onClick={handleClick}
                     >
                         <img
                             src="/resource/Sauce_CFA-Sauce_Desk.png"
                             alt="Chick-Fil-A Sauce"
-                            className="btn-img-cashier-sauce"
+                            className="btn-img-cashier-sauce1"
                         />
-                        <h1>CFAS</h1>
+                        <h4>Chick-fil-A Sauce</h4>
                     </button>
                     <button
-                        className="btn-cashier"
+                        className="btn-sauce"
                         id="pS"
                         onClick={handleClick}
                     >
                         <img
                             src="/resource/Sauce_Polynesian_Desk.png"
                             alt="Polynesian Sauce"
-                            className="btn-img-cashier-sauce"
+                            className="btn-img-cashier-sauce1"
                         />
-                        <h1>PS</h1>
+                        <h4>Polynesian Sauce</h4>
                     </button>
                     <button
-                        className="btn-cashier"
+                        className="btn-sauce"
                         id="hmS"
                         onClick={handleClick}
                     >
                         <img
                             src="/resource/Sauce_Honey-Mustard_Desk.png"
                             alt="Honey Mustard Sauce"
-                            className="btn-img-cashier-sauce"
+                            className="btn-img-cashier-sauce1"
                         />
-                        <h1>HMS</h1>
+                        <h4>Honey Mustard Sauce</h4>
                     </button>
                     <button
-                        className="btn-cashier"
+                        className="btn-sauce"
                         id="ghrS"
                         onClick={handleClick}
                     >
                         <img
                             src="/resource/Sauce_Garden-Herb-Ranch_Desk.png"
                             alt="Garden Herb Ranch Sauce"
-                            className="btn-img-cashier-sauce"
+                            className="btn-img-cashier-sauce1"
                         />
-                        <h1>GHRS</h1>
+                        <h4>Garden Herb Ranch Sauce</h4>
                     </button>
                 </div>
                 <div className="button-row-cashier">
                     <button
-                        className="btn-cashier"
+                        className="btn-sauce"
                         id="bqS"
                         onClick={handleClick}
                     >
                         <img
                             src="/resource/Sauce_Barbeque_Desk.png"
                             alt="Barbeque Sauce"
-                            className="btn-img-cashier-sauce"
+                            className="btn-img-cashier-sauce1"
                         />
-                        <h1>BQS</h1>
+                        <h4>Barbeque Sauce</h4>
                     </button>
                     <button
-                        className="btn-cashier"
+                        className="btn-sauce"
                         id="zbS"
                         onClick={handleClick}
                     >
                         <img
                             src="/resource/Sauce_Zesty-Buffalo_Desk.png"
                             alt="Zesty Buffalo Sauce"
-                            className="btn-img-cashier-sauce"
+                            className="btn-img-cashier-sauce1"
                         />
-                        <h1>ZBS</h1>
+                        <h4>Zesty Buffalo Sauce</h4>
                     </button>
                     <button
-                        className="btn-cashier"
+                        className="btn-sauce"
                         id="srS"
                         onClick={handleClick}
                     >
                         <img
                             src="/resource/Sauce_Sweet-and-Spicy-Sriracha_Desk.png"
                             alt="Sweet and Spicy Sriracha Sauce"
-                            className="btn-img-cashier-sauce"
+                            className="btn-img-cashier-sauce1"
                         />
-                        <h1>SRS</h1>
+                        <h4>Sweet and Spicy Sriracha Sauce</h4>
                     </button>
                     <button
-                        className="btn-cashier"
+                        className="btn-sauce"
                         id="hbqS"
                         onClick={handleClick}
                     >
                         <img
                             src="/resource/Honey-Roasted-BBQ.png"
                             alt="Honey Roasted BBQ Sauce"
-                            className="btn-img-cashier-sauce"
+                            className="btn-img-cashier-sauce1"
                         />
-                        <h1>HBQS</h1>
+                        <h4>Honey Roasted BBQ Sauce</h4>
                     </button>
                 </div>
 
